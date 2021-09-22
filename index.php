2682d5ca70cceeeecc6e3fa9525b6edfe9ed8cbb
Forklein <giuseppepisani2@gmail.com>
2021-09-22 17:50:58 +0200

    Final push

--- index.php
+++ index.php
@@ -3,74 +3,81 @@
 $faq = [
     [
         'title' => 'Come state implementando la recente decisione della Corte di giustizia dell\'Unione europea (CGUE) relativa al diritto all\'oblio?',
-        'description' => 'La recente decisione della Corte di giustizia dell\'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. 
+        'description' => 'La recente <a href="#">decisione della Corte di giustizia dell\'Unione europea</a> ha profonde conseguenze per i motori di ricerca in Europa. 
         La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono
-        il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.</br>
+        il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.</br></br>
         Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo
         valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell\'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire
-        le informazioni.</br>
-        Per presentare una richiesta di rimozione, compila questo modulo web. Riceverai una risposta automatica che conferma la ricezione della tua richiesta. 
+        le informazioni.</br></br>
+        Per presentare una richiesta di rimozione, compila questo <a href="#">modulo web</a>. Riceverai una risposta automatica che conferma la ricezione della tua richiesta. 
         Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po\' di tempo perché abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, 
         considereremo se i risultati includono informazioni obsolete relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico a che le informazioni 
         rimangano nei nostri risultati delle ricerche, ad esempio se riguardano frodi finanziarie, negligenza professionale, condanne penali o la tua condotta pubblica in relazione 
         a un pubblico ufficio (eletto o non eletto). Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo non essere nella posizione giusta per prendere 
-        decisioni in merito al tuo caso. Se non sei d\'accordo con la nostra valutazione, puoi rivolgerti all\'Autorità garante per la protezione dei dati personali nel tuo paese.</br>
+        decisioni in merito al tuo caso. Se non sei d\'accordo con la nostra valutazione, puoi rivolgerti all\'Autorità garante per la protezione dei dati personali 
+        nel tuo paese.</br></br>
         Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità per perfezionare il nostro approccio. La decisione della CGUE 
         rappresenta un cambiamento significativo per i motori di ricerca. Siamo preoccupati per le sue conseguenze, ma riteniamo anche che sia importante rispettare la decisione della 
-        Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge.</br>
+        Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge.</br></br>
         Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati modificati nel rispetto delle leggi europee per la protezione
         dei dati. Mostriamo questa notifica agli utenti europei quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione.'
     ],
     [
         'title' => 'Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?',
         'description' => 'Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un\'elevata sicurezza e assicurarti che
-        le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.
+        le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.</br></br>
         Siamo costantemente al lavoro per garantire un\'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia
         di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. 
         Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci.
-        Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.
-        Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel Centro Google per la sicurezza online.
+        Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.</br></br>
+        Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel <a href="#">Centro Google per la sicurezza online</a>.</br></br>
         Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.'
     ],
     [
         'title' => 'Perché il mio account è associato a un paese?',
         'description' => 'Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:
-        La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. 
-        Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
-        Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell\'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.
-        Google LLC, con sede negli Stati Uniti, per il resto del mondo.
-        La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.
-        Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.
-        <h5>Stabilire il paese associato al tuo account</h5>
+        <ol>
+            <li>La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. 
+            Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:</br></br>
+                <ol type="a">
+                    <li>Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell\'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.</li></br></br>
+                    <li>Google LLC, con sede negli Stati Uniti, per il resto del mondo.</li></br></br>
+                </ol>
+            </li>
+            <li>La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.</li></br></br>
+        </ol>     
+        Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.</br></br>
+        <h5>Stabilire il paese associato al tuo account</h5></br>
         Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese 
-        da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell\'ultimo anno.
+        da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell\'ultimo anno.</br></br>
         I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, potrebbe occorrere circa un anno 
-        per aggiornare l\'associazione del paese.
+        per aggiornare l\'associazione del paese.</br></br>
         Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza tra il paese in cui lavori e il 
         paese in cui risiedi, l\'installazione di una rete privata virtuale (VPN) per mascherare il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. 
-        Contattaci se ritieni che il paese associato al tuo account sia sbagliato.'
+        <a href="#">Contattaci</a> se ritieni che il paese associato al tuo account sia sbagliato.'
     ],
     [
         'title' => 'Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?',
         'description' => 'I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. 
         I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere 
-        i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. 
+        i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi contattare il <a href="#">webmaster</a> del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. 
         Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di 
-        ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l\'aggiornamento, le informazioni non verranno più visualizzate 
-        nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni.'
+        ricerca di Google, <a href="#">fai clic qui</a>. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l\'aggiornamento, le informazioni non verranno più visualizzate 
+        nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile <a href="#">visitare la nostra pagina di assistenza per avere ulteriori informazioni</a>.'
     ],
     [
         'title' => 'Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?',
         'description' => 'In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione 
-        anche l\'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l\'URL della pagina dei risultati di ricerca potrebbe contenere 
+        anche l\'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di <a href="#">URL referrer</a>. Talvolta, l\'URL della pagina dei risultati di ricerca potrebbe contenere 
         la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati 
         come parte dell\'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono 
-        disponibili qui. Le query di ricerca o le informazioni contenute nell\'URL referrer potrebbero essere disponibili mediante Google Analytics o un\'API (Application Programming Interface). 
+        disponibili <a href="#">qui</a>. Le query di ricerca o le informazioni contenute nell\'URL referrer potrebbero essere disponibili mediante Google Analytics o un\'API (Application Programming Interface). 
         Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all\' esatte parole chiave che hanno determinato il clic su un annuncio.'
     ]
 ];
 
 $nav = ['Introduzione', 'Norme sulla privacy', 'Termini di servizio', 'Tecnologie', 'Domande frequenti'];
+$nav_footer = ['Google', 'Tutto su Google', 'Privacy', 'Termini'];
 
 ?>
 
@@ -95,7 +102,9 @@ $nav = ['Introduzione', 'Norme sulla privacy', 'Termini di servizio', 'Tecnologi
             <div class="col">
                 <img style="width: 75px;" src="./images/logo.png" alt="logo">
             </div>
-            <div class="col-2">test</div>
+            <div class="col-2">
+                <strong>Forklein</strong>
+            </div>
         </div>
         <div class="row">
             <nav>
@@ -122,10 +131,22 @@ $nav = ['Introduzione', 'Norme sulla privacy', 'Termini di servizio', 'Tecnologi
         </div>
     </main>
 
-    <footer class="container-fluid bg-secondary">
-        <div class="row">
-            <div class="col-6">aaa</div>
-            <div class="col-6">aaa</div>
+    <footer class="container-fluid">
+        <div class="row align-items-center">
+            <div class="col-6">
+                <div class="d-flex justify-content-center">
+                    <ul class="d-flex list-unstyled">
+                        <?php foreach ($nav_footer as $menu) : ?>
+                            <li class="pe-3 pointer text-secondary"><?php echo $menu ?></li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-6 text-center">
+                <select>
+                    <option>Italiano</option>
+                </select>
+            </div>
         </div>
     </footer>
 
